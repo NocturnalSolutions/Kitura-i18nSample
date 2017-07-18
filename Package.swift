859881683201f52bb81fe7a9933e75bd289ce0a6
Nocturnal <contact@nocturnal.solutions>
2017-07-17 23:05:27 -0600

    Add packages to Package.swift

--- Package.swift
+++ Package.swift
@@ -3,5 +3,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "KituraI18nSample"
+    name: "KituraI18nSample",
+    dependencies: [
+        .Package(url: "https://github.com/NocturnalSolutions/Kitura-Translation.git", majorVersion: 0),
+        .Package(url: "https://github.com/NocturnalSolutions/Kitura-LanguageNegotiation.git", majorVersion: 0)
+    ]
 )
