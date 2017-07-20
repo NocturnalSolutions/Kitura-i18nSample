d667feefe0c5edc032ca9598ff3d4db18a1ccbca
Nocturnal <contact@nocturnal.solutions>
2017-07-19 23:01:14 -0600

    Initial working example.

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,42 @@
-print("Hello, world!")
+import Kitura
+import KituraLangNeg
+import LoggerAPI
+import HeliumLogger
+import KituraTranslation
+import Foundation
+
+HeliumLogger.use()
+
+guard let ln = try? LanguageNegotiation(["en", "ja", "es", "zh-hans"], methods: .pathPrefix) else {
+    Log.error("Something went horribly wrong.")
+    exit(1)
+}
+
+let poDir = URL(fileURLWithPath: #file + "/../../Translations").standardizedFileURL.path
+
+let settings = TranslationSettings(lang: "en", poDir: poDir)
+Translation.settings = settings
+
+let router = Router()
+
+router.get("/") { request, response, next in
+    defer {
+        next()
+    }
+
+    if request.userInfo["LangNeg"] == nil {
+        Translation.settings!.lang = "en"
+    }
+    else {
+        let match = request.userInfo["LangNeg"] as! LanguageNegotiation.NegMatch
+        Translation.settings!.lang = match.lang
+    }
+    response.send("Hello!".t())
+    response.send("\n")
+}
+
+let parentRouter = Router()
+parentRouter.all(ln.routerPaths, middleware: [ln, router])
+
+Kitura.addHTTPServer(onPort: 8080, with: parentRouter)
+Kitura.run()
